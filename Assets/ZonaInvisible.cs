37adc040158e5101cf6a0c6ce9b81799b255765c
Sofia Amaya <laura18amaya@gmail.com>
2025-01-31 00:04:48 -0500

    kimites

--- Assets/ZonaInvisible.cs
+++ Assets/ZonaInvisible.cs
@@ -2,17 +2,33 @@ using UnityEngine;
 
 public class ZonaInvisible : MonoBehaviour
 {
-    public Vector3 puntoRestriccion = new Vector3(0f, 0f, 0f); // El punto hasta donde se puede mover el jugador
+    private PlayerMovement playerMovement;  
 
-    // Este método se llama cuando otro objeto entra en la zona
-    private void OnTriggerEnter(Collider other)
+    void Start()
     {
-        if (other.CompareTag("Player")) // Verificamos si el objeto es el jugador
+        
+        playerMovement = GameObject.FindWithTag("Player").GetComponent<PlayerMovement>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))  
         {
-            // Aquí puedes poner la lógica para detener al jugador.
-            // Esto puede ser simplemente detener su movimiento o devolverlo a una posición válida.
-            other.transform.position = puntoRestriccion; // Mueve al jugador de vuelta al punto de restricción
+            if (playerMovement != null)
+            {
+                playerMovement.velocidad = 0;  
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))  
+        {
+            if (playerMovement != null)
+            {
+                playerMovement.velocidad = 8.0f;  
+            }
         }
     }
 }
-
